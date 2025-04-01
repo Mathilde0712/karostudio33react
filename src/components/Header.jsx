67ac8dc88513c037c9870ff987fd51dde0a17028
Mathilde Besse <mathildebesse07@gmail.com>
2025-04-01 16:19:58 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+import logo from "../assets/photos/Karostudiologo1.png"
+const Header = () => {
+  return (
+    <div className="header">
+       
+      <img src={logo} alt="logo" className="logo"/>
+      <div className="content">
+      <h1 className="header-text">Vos précieux souvenirs</h1>
+      <h1 className="header-text">capturés pour toujours</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
