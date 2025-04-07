e8e4dfbc6e64ea5ee8e990b43cf745eeff91b615
Mathilde Besse <mathildebesse07@gmail.com>
2025-04-07 14:09:48 +0200

    hero ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,25 @@
-import HeroKaro from "../assets/photos/heroKaro.webp"
+import HeroKaro from "../assets/photos/heroKaro.webp";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-content">
-      <h1 className="hero-title">À mon sujet</h1>
+        <h1 className="hero-title">À mon sujet</h1>
         <div className="Herocontent">
-        <p className="hero-text">
-          Maman de deux enfants, la photographie c’est avant tout une passion
-          mais aussi le challenge d’une nouvelle aventure.
-        </p>
-        <p className="hero-text">
-          Mon style est lumineux et épuré dans la douceur de tons beige et
-          blanc. Je capture tous les moments importants de votre vie
-        </p>
+          <p className="hero-text1">
+          Maman de deux enfants, la photographie est avant tout une passion, mais aussi le défi d’une nouvelle aventure.
+
+
+          </p>
+          <p className="hero-text">
+          Mon style est lumineux et épuré, dans la douceur des tons beige et blanc. Je capture les moments importants de votre vie avec sensibilité et authenticité.
+          </p>
+          <p className="hero-text">
+          Je veille à ce que vous vous sentiez détendu(e) et à l’aise tout au long de la séance, en créant une atmosphère calme et chaleureuse..
+          </p>
+        </div>
       </div>
-    </div>
-    <img className="heroImg" src={HeroKaro} alt="une mariée"/>
+      <img className="heroImg" src={HeroKaro} alt="une mariée" />
     </div>
   );
 };
