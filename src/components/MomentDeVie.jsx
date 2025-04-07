d0752e3cbf18b6328fee16c5c97c97c4718b13f0
Mathilde Besse <mathildebesse07@gmail.com>
2025-04-07 15:55:42 +0200

    maj moment et evenements de vie

--- src/components/MomentDeVie.jsx
+++ src/components/MomentDeVie.jsx
@@ -2,64 +2,68 @@ import React from 'react';
 import grossessePhoto from "../assets/photos/grossesse.webp";
 import couplePhoto from "../assets/photos/couple.webp";
 import naissancePhoto from "../assets/photos/headerKaro.webp";
-import famillePhoto from "../assets/photos/famille.webp"
+import famillePhoto from "../assets/photos/famille.webp";
+import mariage2 from "../assets/photos/mariage2.webp";
+import pro from "../assets/photos/pro.webp";
+import evjf from "../assets/photos/evjf-julie-1-12-_2_.webp";
+
 
 const MomentDeVie = () => {
     return (
         <div>
-        <div className='momentDeVie'>
+        <div className='momentDeVie momentDeVie1'>
             <h1 className='title-moment'>MOMENTS DE VIE</h1>
             <div className='content-moment'>
                 <div className='moment'>
                     <img src={grossessePhoto} alt='grossesse'/>
-                    <h2>Grossesse</h2>
+                    <h2 className='momentTitle'>Grossesse</h2>
                 </div>
                 <div className='moment'>
                     <img src={naissancePhoto} alt='naissance' />
-                    <h2>Naissance</h2>
+                    <h2 className='momentTitle' >Naissance</h2>
                 </div>
                 <div className='moment'>
                     <img src={famillePhoto} alt='famille'/>
-                    <h2>Famille</h2>
+                    <h2  className='momentTitle'>Famille</h2>
                 </div>
                 <div className='moment'>
                     <img src={couplePhoto} alt='couple'/>
-                    <h2>Couple</h2>
+                    <h2 className='momentTitle' >Couple</h2>
                 </div>
             </div>
         </div>
-        <div className='momentDeVie'>
+        <div className='momentDeVie momentDeVie2'>
             <h1 className='title-moment'>ÉVÈNEMENTS DE VIE</h1>
             <div className='content-moment'>
                 <div className='moment'>
-                    <img src={grossessePhoto} alt='grossesse'/>
-                    <h2>Anniversaire</h2>
+                    <img src={grossessePhoto} alt='anniversaire'/>
+                    <h2  >Anniversaire</h2>
                 </div>
                 <div className='moment'>
-                    <img src={naissancePhoto} alt='naissance' />
+                    <img src={mariage2} alt='mariage' />
                     <h2>Mariage</h2>
                 </div>
                 <div className='moment'>
-                    <img src={famillePhoto} alt='famille'/>
+                    <img src={famillePhoto} alt='baptême'/>
                     <h2>Baptême</h2>
                 </div>
                 
             </div>
         </div>
-        <div className='momentDeVie'>
+        <div className='momentDeVie momentDeVie3'>
             <h1 className='title-moment'>ÉVÈNEMENTS DE VIE</h1>
             <div className='content-moment'>
                 <div className='moment'>
-                    <img src={grossessePhoto} alt='grossesse'/>
-                    <h2>Gender Reveal</h2>
+                    <img src={grossessePhoto} alt='gender reveal'/>
+                    <h2 className='momentTitle' >Gender Reveal</h2>
                 </div>
                 <div className='moment'>
-                    <img src={naissancePhoto} alt='naissance' />
-                    <h2>Evjf</h2>
+                    <img src={evjf} alt='evjf' />
+                    <h2 className='momentTitle'>Evjf</h2>
                 </div>
                 <div className='moment'>
-                    <img src={famillePhoto} alt='famille'/>
-                    <h2>Professionnel</h2>
+                    <img src={pro} alt='professionnel'/>
+                    <h2 className='momentTitle' >Professionnel</h2>
                 </div>
             </div>
         </div>
