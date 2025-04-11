2146d225268ae7efa86c82df97af2da1efd7e216
Mathilde Besse <mathildebesse07@gmail.com>
2025-04-11 14:38:29 +0200

    maj composant tarifs

--- src/components/Tarifs.jsx
+++ src/components/Tarifs.jsx
@@ -1,3 +1,4 @@
+
 import famillePhoto from "../assets/photos/famille.webp";
 import couplePhoto from "../assets/photos/couple.webp";
 import mariage2 from "../assets/photos/mariage2.webp";
@@ -8,16 +9,19 @@ const Tarifs = () => {
         <h1 className="title-tarif">TARIFS</h1>
         <div className="content-tarif">
           <div className="tarifcontent">
+          <div className="separator1" >
             <h2 className="tarifTitle">Bulle de bonheur </h2>
             <p>10 photos numériques</p>
             <p>accessoires et pret de tenue</p>
             <p className="text1" >home studio ou extérieur</p>
             <p className="prix">130 euros</p>
             <img src={couplePhoto} alt="couple" />
+            </div>
           </div>
           
          
-          <div className="tarifcontent">
+          <div className="tarifcontent ">
+            <div className="separator2" >
             <h2 className="tarifTitle">Jolie merveille </h2>
             <p>20 photos numériques</p>
             <p>accessoires et pret de tenue</p>
@@ -25,15 +29,18 @@ const Tarifs = () => {
             <p className="prix">180 euros</p>
             <img src={mariage2} alt="mariée" />
             </div>
+            </div>
           
       
           <div className="tarifcontent">
+          <div className="separator3" >
             <h2 className="tarifTitle">Sur un nuage </h2>
             <p>30 photos numériques</p>
             <p>accessoires et pret de tenue</p>
             <p className="text1">home studio ou extérieur</p>
             <p className="prix">230 euros</p>
             <img src={famillePhoto} alt="famille" />
+            </div>
           </div>
       </div>
       <p className="cartekdo" >Pour offrir une carte cadeau, contactez moi !</p>
