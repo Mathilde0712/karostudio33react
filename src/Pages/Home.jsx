3db460c49be2b318faff446adc34f9009e4219eb
Mathilde Besse <mathildebesse07@gmail.com>
2025-05-06 16:33:09 +0200

    mentions legales

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import MomentDeVie from "../components/MomentDeVie";
 import Tarifs from "../components/Tarifs";
-import Footer from "../components/Footer";
+
 
 const Home = () => {
     return (
