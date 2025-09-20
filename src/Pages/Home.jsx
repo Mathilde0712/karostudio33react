b16a4e37bda4e5bd3d67d23c75585b9c6f643321
Mathilde Besse <mathildebesse07@gmail.com>
2025-09-20 19:13:56 +0200

    maj

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,7 @@ import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import MomentDeVie from "../components/MomentDeVie";
-import Tarifs from "../components/Tarifs";
-import Tarifs2 from "../components/Tarifs2";
+
 
 const Home = () => {
   return (
